eabad63ff95c751e344c41dd953628c6c6efb555
y9858 <53130835+y9858@users.noreply.github.com>
2022-01-05 14:50:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,19 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# Add luci-app-ssr-plus
+git clone https://github.com/fw876/helloworld package/openwrt-ssrplus
+
+# Add luci-app-dockerman
+git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
+
+# Add luci-app-adblock-plus
+git clone https://github.com/small-5/luci-app-adblock-plus package/luci-app-adblock-plus
+
+# Add luci-app-amlogic
+git clone https://github.com/ophub/luci-app-amlogic package/luci-app-amlogic
+
+# Add autocore support for armvirt
+sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
