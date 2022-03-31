5ea5cee865b6b85d6ae89f16b4f210de43ca06a8
y9858 <53130835+y9858@users.noreply.github.com>
2022-03-31 09:36:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,8 +11,8 @@
 #
 
 # Modify default theme
-rm -rf feeds/luci/themes
-git clone https://github.com/y9858/themes feeds/luci/themes
+rm -rf feeds/luci/themes/*
+svn co https://github.com/y9858/themes/trunk feeds/luci/themes
 #sed -i 's/luci-theme-bootstrap/luci-theme-atmaterial/g' ./feeds/luci/collections/luci/Makefile
 
 # Modify default IP
