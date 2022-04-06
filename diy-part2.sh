2cb5b56960205790f82eeea696646fc814107b5c
y9858 <53130835+y9858@users.noreply.github.com>
2022-04-06 17:12:54 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,9 +17,8 @@ sed -i 's/luci-theme-bootstrap/luci-theme-opentomcat/g' ./feeds/luci/collections
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-# Add luci-app-v2raya
-git clone https://github.com/zxlhhyccc/luci-app-v2raya package/luci-app-v2raya
-git clone https://github.com/v2rayA/v2raya-openwrt package/v2raya-openwrt
+# Add v2raya
+svn co https://github.com/fw876/helloworld/trunk/v2raya package/v2raya
 
 # Add luci-app-dockerman
 cd feeds/luci/applications
