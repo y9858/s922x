4a19c04130303b0ac7c7611ea99e5b75bb46d6cf
y9858 <53130835+y9858@users.noreply.github.com>
2022-04-07 08:22:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,9 +16,8 @@ sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/l
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-# Add luci-app-ssr-plus
-git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
-git clone https://github.com/y9858/luci-app-ssr-plus package/luci-app-ssr-plus
+# Add luci-app-passwall
+git clone https://github.com/y9858/luci-app-passwall package/luci-app-passwall
 
 # Add luci-app-mosdns
 git clone https://github.com/QiuSimons/openwrt-mos package/luci-app-mosdns
