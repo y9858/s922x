4315aa4710a6266b85d18773e07008400d170ded
y9858 <53130835+y9858@users.noreply.github.com>
2022-04-11 22:04:54 +0800

    Delete lean.sh

--- lean.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#
-
-echo 'src-git darky https://github.com/solidus1983/-luci-theme-darky.git' >>feeds.conf.default
-echo 'src-git opentomato https://github.com/solidus1983/luci-theme-opentomato.git' >>feeds.conf.default
