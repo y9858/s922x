47c526a64c831ae3cdad284facff114442098549
y9858 <53130835+y9858@users.noreply.github.com>
2022-04-11 19:20:01 +0800

    Create lean.sh

---
+++ lean.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+git clone https://github.com/coolsnowwolf/lede
+
+echo 'src-git darky https://github.com/solidus1983/-luci-theme-darky.git' >>feeds.conf.default
+echo 'src-git opentomato https://github.com/solidus1983/luci-theme-opentomato.git' >>feeds.conf.default
