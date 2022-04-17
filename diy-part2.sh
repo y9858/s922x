a53a5704a090b9cd8c45dd221e4fc6717c04529d
y9858 <53130835+y9858@users.noreply.github.com>
2022-04-17 12:57:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,9 +11,10 @@
 #
 
 # Modify default theme
-git clone https://github.com/toss-a/luci-theme-atmaterial package/luci-theme-atmaterial
-sed -i '529d' package/luci-theme-atmaterial/files/htdocs/css/style.css
-sed -i 's/luci-theme-bootstrap/luci-theme-atmaterial/g' ./feeds/luci/collections/luci/Makefile
+rm -rf feeds/luci/themes/luci-theme-material
+git clone https://github.com/LuttyYang/luci-theme-material package/luci-theme-material
+sed -i 's/9. Themes/4. Themes/g' package/luci-theme-material/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/luci/Makefile
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
@@ -25,18 +26,8 @@ sed -i 's/xray/v2ray/g' package/luci-app-ssr-plus/luci-app-ssr-plus/Makefile
 sed -i '6d' package/luci-app-ssr-plus/luci-app-ssr-plus/luasrc/model/cbi/shadowsocksr/client.lua
 sed -i '5a m = Map("shadowsocksr", translate("ShadowSocksR Plus+ Settings"))' package/luci-app-ssr-plus/luci-app-ssr-plus/luasrc/model/cbi/shadowsocksr/client.lua
 
-# Add luci-app-dockerman
-cd feeds/luci/applications
-rm -rf luci-app-dockerman
-cd ../../..
-git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
-
 # Add luci-app-unblockneteasemusic
-cd feeds/luci/applications
-rm -rf luci-app-unblockmusic
-cd ../../..
 git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic package/luci-app-unblockneteasemusic
-sed -i 's/解除网易云音乐播放限制/解除网易云音乐限制/g' package/luci-app-unblockneteasemusic/luasrc/controller/unblockneteasemusic.lua
 
 # Add luci-app-amlogic
 git clone https://github.com/ophub/luci-app-amlogic package/luci-app-amlogic
