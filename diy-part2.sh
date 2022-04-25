6f4349de5c67f2231622758f269a7d2cfab4098a
y9858 <53130835+y9858@users.noreply.github.com>
2022-04-25 17:05:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,6 +21,7 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 
 # Add luci-app-v2raya
 git clone https://github.com/zxlhhyccc/luci-app-v2raya package/luci-app-v2raya
+sed -i 's/+v2raya/+v2raya +ip-full +xray-core/g' package/luci-app-v2raya/Makefile
 git clone https://github.com/v2rayA/v2raya-openwrt package/v2raya-openwrt
 
 # Add luci-app-adguardhome
