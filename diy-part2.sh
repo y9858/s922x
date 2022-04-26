ae7af93720491fe3a7c8467611d02f0118d2ef98
y9858 <53130835+y9858@users.noreply.github.com>
2022-04-26 10:05:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,10 +19,10 @@ sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/l
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-# Add luci-app-v2raya
-git clone https://github.com/zxlhhyccc/luci-app-v2raya package/luci-app-v2raya
-sed -i 's/+v2raya/+v2raya +ip-full +xray-core/g' package/luci-app-v2raya/Makefile
-git clone https://github.com/v2rayA/v2raya-openwrt package/v2raya-openwrt
+# Add luci-app-ssr-plus
+git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
+sed -i '6d' package/luci-app-ssr-plus/luci-app-ssr-plus/luasrc/model/cbi/shadowsocksr/client.lua
+sed -i '5a m = Map("shadowsocksr", translate("ShadowSocksR Plus+ Settings"))' package/luci-app-ssr-plus/luci-app-ssr-plus/luasrc/model/cbi/shadowsocksr/client.lua
 
 # Add luci-app-adguardhome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
