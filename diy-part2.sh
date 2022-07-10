3cef6f045fc6b0b3d2d756c3c8c07839d66594b1
y9858 <53130835+y9858@users.noreply.github.com>
2022-07-10 10:07:00 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,10 +19,10 @@ sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/l
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-# Add luci-app-ssr-plus
-git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
-sed -i '8d' package/luci-app-ssr-plus/luci-app-ssr-plus/luasrc/model/cbi/shadowsocksr/client.lua
-sed -i '7a m = Map("shadowsocksr", translate("ShadowSocksR Plus+ Settings"))' package/luci-app-ssr-plus/luci-app-ssr-plus/luasrc/model/cbi/shadowsocksr/client.lua
+# Add luci-app-v2raya
+git clone https://github.com/zxlhhyccc/luci-app-v2raya package/luci-app-v2raya
+git clone https://github.com/v2rayA/v2raya-openwrt package/v2raya-openwrt
+sed -i 's/+v2raya/+v2raya +ip-full +v2fly-geoip +v2fly-geosite +v2ray-core +v2ray-ctl +v2ray-extra/g' package/luci-app-v2raya/Makefile
 
 # Add luci-app-dockerman
 rm -rf feeds/luci/applications/luci-app-dockerman
