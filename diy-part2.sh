6d28e952df916e6f73ba137562d9acd8dc587e3b
y9858 <53130835+y9858@users.noreply.github.com>
2022-11-11 08:05:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,6 +22,9 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 # ttyd 免帐号登录
 sed -i 's/\/bin\/login/\/bin\/login -f root/' feeds/packages/utils/ttyd/files/ttyd.config
 
+# Modify luci-app-unblockmusic
+sed -i 's/解锁网易云灰色歌曲/网易云音乐/g' feeds/luci/applications/luci-app-unblockmusic/po/zh-cn/unblockmusic.po
+
 # Add luci-app-passwall
 svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall package/luci-app-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
@@ -30,8 +33,13 @@ git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
 rm -rf feeds/luci/applications/luci-app-dockerman
 svn co https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman package/luci-app-dockerman
 
+# Add luci-app-mosdns
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+git clone https://github.com/QiuSimons/openwrt-mos package/openwrt-mos
+
 # Add luci-app-dnsfilter
-git clone https://github.com/kiddin9/luci-app-dnsfilter package/luci-app-dnsfilter
+#git clone https://github.com/kiddin9/luci-app-dnsfilter package/luci-app-dnsfilter
 
 # Add luci-app-amlogic
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
