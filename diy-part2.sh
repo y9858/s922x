bcdaf79deb4ffc8712f8338ad85ac7fe62060b48
y9858 <53130835+y9858@users.noreply.github.com>
2023-11-16 19:35:54 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,8 +11,7 @@
 #
 
 # Modify default theme
-git clone https://github.com/y9858/luci-theme-opentomcat package/luci-theme-opentomcat
-sed -i 's/luci-theme-bootstrap/luci-theme-opentomcat/g' ./feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/luci-light/Makefile
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
@@ -20,15 +19,15 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 # ttyd 免帐号登录
 sed -i 's/\/bin\/login/\/bin\/login -f root/' feeds/packages/utils/ttyd/files/ttyd.config
 
-# Add luci-app-v2raya
-git clone -b 18.06 https://github.com/zxlhhyccc/luci-app-v2raya package/v2raya-openwrt/luci-app-v2raya
-svn co https://github.com/v2rayA/v2raya-openwrt/trunk/v2raya package/v2raya-openwrt/v2raya
-svn co https://github.com/v2rayA/v2raya-openwrt/trunk/v2ray-core package/v2raya-openwrt/v2ray-core
-svn co https://github.com/v2rayA/v2raya-openwrt/trunk/v2fly-geodata package/v2raya-openwrt/v2fly-geodata
+# bash 替换 ash
+[ ! -e "/bin/bash" ] || sed -i "23i sed -i '1s/ash/bash/' /etc/passwd" package/emortal/default-settings/files/99-default-settings
 
-# Add luci-app-unblockneteasemusic
-git clone -b master https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic package/luci-app-unblockneteasemusic
-sed -i 's/解除网易云音乐播放限制/音乐解锁/g' package/luci-app-unblockneteasemusic/luasrc/controller/unblockneteasemusic.lua
+# Modify appname
+sed -i 's/解除网易云音乐播放限制/音乐解锁/g' feeds/luci/applications/luci-app-unblockneteasemusic/root/usr/share/luci/menu.d/luci-app-unblockneteasemusic.json
+
+# Modify v2raya
+sed -i 's/xray/v2ray/g' feeds/packages/net/v2raya/Makefile
 
 # Add luci-app-amlogic
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+sed -i 's/+block-mount/+libuci-lua +lua +luci-compat +luci-lib-jsonc +block-mount/g' package/luci-app-amlogic/Makefile
