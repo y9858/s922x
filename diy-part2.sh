0461c469953aa52b5b148da8accc283c435f8c8f
y9858 <53130835+y9858@users.noreply.github.com>
2023-11-17 07:48:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,13 +20,14 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 sed -i 's/\/bin\/login/\/bin\/login -f root/' feeds/packages/utils/ttyd/files/ttyd.config
 
 # bash 替换 ash
-[ ! -e "/bin/bash" ] || sed -i "23i sed -i '1s/ash/bash/' /etc/passwd" package/emortal/default-settings/files/99-default-settings
+#sed -i "23i sed -i '1s/ash/bash/' /etc/passwd" package/emortal/default-settings/files/99-default-settings
 
 # Modify appname
 sed -i 's/解除网易云音乐播放限制/音乐解锁/g' feeds/luci/applications/luci-app-unblockneteasemusic/root/usr/share/luci/menu.d/luci-app-unblockneteasemusic.json
 
 # Modify v2raya
 sed -i 's/xray/v2ray/g' feeds/packages/net/v2raya/Makefile
+sed -i 's/+v2ray-core/+v2ray-core +v2ray-geoip +v2ray-geosite/g' feeds/packages/net/v2raya/Makefile
 
 # Add luci-app-amlogic
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
