a1002c7bd154928db3c8dfd73490c03a4ab4cb7e
y9858 <53130835+y9858@users.noreply.github.com>
2024-03-09 17:42:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,8 +20,8 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 # ttyd 免帐号登录
 sed -i 's/\/bin\/login/\/bin\/login -f root/' feeds/packages/utils/ttyd/files/ttyd.config
 
-# Add luci-app-passwall
-git clone -b luci-smartdns-dev --depth 1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+# Add luci-app-passwall2
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
 
 # Add luci-app-openclash
