2a55b30f5e24b87913f440432d7cc13f745999ae
y9858 <53130835+y9858@users.noreply.github.com>
2024-04-10 21:29:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,8 +11,7 @@
 #
 
 # Modify default theme
-git clone --depth 1 https://github.com/y9858/luci-theme-opentomcat package/luci-theme-opentomcat
-sed -i 's/luci-theme-bootstrap/luci-theme-opentomcat/g' ./feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/luci-light/Makefile
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
@@ -20,12 +19,11 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 # ttyd 免帐号登录
 sed -i 's/\/bin\/login/\/bin\/login -f root/' feeds/packages/utils/ttyd/files/ttyd.config
 
-# Add luci-app-passwall
-git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
-git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
+# bash 替换 ash
+sed -i "23i sed -i '1s/ash/bash/' /etc/passwd" package/emortal/default-settings/files/99-default-settings
 
-# Add luci-app-openclash
-git clone -b dev --depth 1 https://github.com/vernesong/OpenClash package/luci-app-openclash
+# Modify appname
+sed -i 's/解除网易云音乐播放限制/音乐解锁/g' feeds/luci/applications/luci-app-unblockneteasemusic/root/usr/share/luci/menu.d/luci-app-unblockneteasemusic.json
 
 # Add luci-app-mosdns
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
@@ -33,11 +31,6 @@ find ./ | grep Makefile | grep mosdns | xargs rm -f
 find ./ | grep Makefile | grep v2dat | xargs rm -f
 git clone --depth 1 https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone --depth 1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-
-# Add luci-app-unblockneteasemusic
-sed -i 's/default NODEJS_16/default NODEJS_18/g' feeds/packages/lang/node/Makefile
-git clone -b master --depth 1 https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic package/luci-app-unblockneteasemusic
-sed -i 's/解除网易云音乐播放限制/音乐解锁/g' package/luci-app-unblockneteasemusic/luasrc/controller/unblockneteasemusic.lua
 
 # Add luci-app-amlogic
 git clone --depth 1 https://github.com/ophub/luci-app-amlogic package/luci-app-amlogic
